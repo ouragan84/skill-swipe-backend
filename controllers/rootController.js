a6f0a15247c675cac534bfd645aceb344628c23e
Edgar Baudry <edgar.baudry@gmail.com>
2023-02-03 21:25:48 -0800

    added git commit hash endpoint

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const rootController = express();
 
+git_commit_hash = require('child_process')
+  .execSync('git rev-parse HEAD')
+  .toString().trim();
+
 // Define the home page route
 rootController.get('/', function(req, res) {
   res.send('Hello From Skill Swipe Backend API!');
@@ -10,6 +14,11 @@ rootController.get('/', function(req, res) {
 // Define the about route
 rootController.get('/about', function(req, res) {
   res.send('About us');
+});
+
+// To get commit hash
+rootController.get('/git-commit', function(req, res) {
+  res.send(git_commit_hash);
 });
 
 module.exports = rootController;
